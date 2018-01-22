4ca6d3b27578d4931fb00e07d40cb337ebc86ae9
homahi <homahi0128@gmail.com>
2018-01-22 18:42:37 +0900

    dataの中から値を取り出して配列化してみた

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,14 +13,16 @@ export class AppComponent implements OnInit {
   constructor(private service: RxjsSampleService) { }
   title = 'app';
   data: any;
+  data2: any;
 
   ngOnInit() {
-    let a = this.service.get();
-    let b = this.service.get2();
-    let c = this.service.get();
+    let a = this.service.get().map(item => {return item.hoge});
+    let b = this.service.get2().map(item => {return item.hoge});
+    let array = [a,b]
 
-    Observable.forkJoin([a, b, c]).subscribe((data) => {
-      this.data = Array.prototype.concat.apply([],data);
+    Observable.forkJoin([a, b]).map(item =>{return Array.prototype.concat.apply([],item)}).subscribe((data) => {
+      this.data = data.map(item => {return item.fuga});
     });
+
   }
 }
